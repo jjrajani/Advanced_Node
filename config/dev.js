62314cb388b973b9683cb7600a5b6376a734415d
Jenna Rajani <jjrajani@gmail.com>
2018-04-02 19:55:34 -0400

    hide dev keys

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://jjrajani:advancedredux@ds123929.mlab.com:23929/advanced_redux_redix',
-  cookieKey: 'dsafdfwqeasdfq3regdfsf',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
